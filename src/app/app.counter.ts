1a2cc96bae18133fb803e3c4f6c1240a575e4f8b
Igor Smorodsky <ingerard@gmail.com>
2022-06-29 17:10:36 +0300

    Update app.counter.ts

--- src/app/app.counter.ts
+++ src/app/app.counter.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
 
+const numbers = interval(1000);
+
 @Component({
     selector: 'purchase-app',
     template: `<div class="page-header">
         <h1> Счетчик </h1>
     </div>
     <div class="counter-wrapper">
-            <h1 class="counter-display">{{secondsToDday}}</h1>
+            <h1 class="counter-display">{{number}}</h1>
             <button class="counter-start">Start</button>
             <button class="counter-stop">Stop</button>    
             <button class="counter-stop">Wait</button>
@@ -15,38 +17,9 @@ import { Subscription, interval } from 'rxjs';
 })
 export class Counter {
 
-
-    private subscription: Subscription;
-  
-    public dateNow = new Date();
-    public dDay = new Date('Jan 01 2023 00:00:00');
-    milliSecondsInASecond = 1000;
-    hoursInADay = 24;
-    minutesInAnHour = 60;
-    SecondsInAMinute  = 60;
-
-    public timeDifference;
-    public secondsToDday;
-    public minutesToDday;
-    public hoursToDday;
-    public daysToDday;
-
-
-    private getTimeDifference () {
-        this.timeDifference = this.dDay.getTime() - new  Date().getTime();
-        this.allocateTimeUnits(this.timeDifference);
-    }
-
-    private allocateTimeUnits (timeDifference) {
-        this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
-        this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
-        this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
-        this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
-  }
+    number;
 
   ngOnInit() {
-    this.subscription = interval(1000)
-        .subscribe(x => { this.getTimeDifference(); });
- }
+  }
 
 }
