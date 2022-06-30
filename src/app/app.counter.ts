1eab1e95890e3e772c157df71c25f40e484d3104
Igor Smorodsky <ingerard@gmail.com>
2022-06-30 19:45:20 +0300

    first working commit

--- src/app/app.counter.ts
+++ src/app/app.counter.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { interval, Observable, fromEvent, debounceTime, filter, map, Subject, timer, scan, startWith, BehaviorSubject, mapTo, throttle, throttleTime, count, timeInterval } from 'rxjs';
+import { interval, Observable, fromEvent, debounceTime, filter, map, Subject, timer, scan, startWith, BehaviorSubject, mapTo, throttle, throttleTime, count, timeInterval, takeWhile, switchMap, take } from 'rxjs';
 
 const numbers = interval(1000);
 
@@ -9,93 +9,83 @@ const numbers = interval(1000);
         <h1> Счетчик </h1>
     </div>
     <div class="counter-wrapper">
-            <h1 class="counter-display">{{count}}</h1>
-            <button (click)="getItem($event)" class="counter-start">Start</button>
-            <button (click)="setTimer(0)" class="counter-stop">Stop</button>    
-            <button (click)="setTimer(-1)" class="counter-wait">Wait</button>
+            <h1 class="counter-display">{{mmSS}}</h1>                        
+            <button class="counter-start">Start</button>
+            <button class="counter-stop">Stop</button>    
+            <button class="counter-wait">Wait</button>
     </div>`
 })
 
 export class Counter implements OnInit {
 
-    @ViewChild('btn', { static: true }) button: ElementRef;
+    @ViewChild('btn', { static: true }) button: ElementRef;   
 
     constructor() { }
 
-    setTimer(param: number) {
-
-    }
-    // private buttonClicked = new Subject<string>();
-
-    public getItem(itemId: string) {
-        // this.buttonClicked.next(itemId);
-    }
     // init start num
-    count: number = 0;
+    count: number = 300;
+    // MM:SS format
+    mmSS: string;
+    // Interval of stream timer
     interval: number = 1000;
-    clickInterval: number = 500;
-
-    btnSelector = document.querySelector('.counter-start');
-
-
-    renderCount(num: number){
-        this.count = num;
+    // Interval between mouse click
+    clickInterval: number = 500;    
+    
+    // Convert sec to MM:SS
+    convertSec(sec: number): void {
+        this.mmSS = new Date(sec * 1000).toISOString().substring(14, 19);
     }
-    // watch = new Observable(observer => {
-
-    //     observer.next(this.count);
-
-    //     const watchInterval = setInterval(() => {
-    //         this.count += 1;
-    //         observer.next(this.count);
-    //     }, 1000);
-
-    //     return () => clearInterval(watchInterval);
-    // }).pipe(
-    //     filter(data => data > 3)
-    // );
-
-    // subscribe = example.subscribe(val => console.log(val));
-    // stopwatchSubscription = this.watch.subscribe(count => {
-    //     // console.log(count);
-    // });
-
-    // buttonSubscription =  fromEvent(document.querySelector('.counter-start'), 'click')
-    //         .pipe(debounceTime(300))
-    //         .subscribe(res => console.log(res));
-
+    
 
     ngOnInit() {
-        
-        // Count stream
-        const counter$ = timer(0, 1000);
-        // Mouse clicks stream
-        const clickStream$ = fromEvent(this.btnSelector, 'click');
-        // const toggle$ = new BehaviorSubject(true);
 
-        // 
-        let doubleClick$ = clickStream$.pipe(
+        // Selectors for button elements
+        const btnStartSelector = document.querySelector('.counter-start');
+        const btnStopSelector = document.querySelector('.counter-stop');
+        const btnWaitSelector = document.querySelector('.counter-wait');
+        
+        // Mouse clicks stream
+        const btnStart$ = fromEvent(btnStartSelector, 'click');
+        const btnStop$ = fromEvent(btnStopSelector, 'click');
+        const btnWait$ = fromEvent(btnWaitSelector, 'click');
+        
+        // INIT count stream
+        const counter$ = timer(0, this.interval)
+        .pipe(            
+            mapTo(1),
+            map(x => this.count -= x),
+            takeWhile(x => x >= 0)
+        );
+        
+        // INIT "Wait" button stream
+        let btnWaitStream$ = btnWait$.pipe(
             timeInterval(),
             scan((acc, val) => val.interval < this.clickInterval ? acc + 1 : 0, 0),
             filter(val => val == 1)
         );
 
-        // Subscribes to stream
-        doubleClick$.subscribe((e) => console.log('Pause the timer'));
-        // counter$.subscribe((timeLeft) => this.renderCount(timeLeft));
-        counter$.subscribe((v) => console.log(v));
+        
+        let counterSub;
 
-        // const buttonClickedDebounced = this.buttonClicked.pipe(
-        //     scan((acc, cur) => {
-        //         acc + cur;
-        //         console.log(acc + cur);
-        //     }, true), startWith(true)
-        // );
+        // Subscriber to "Start" button
+        btnStart$.subscribe(() => {
+            this.count = 120;
+            counterSub = counter$.subscribe((timeLeft) => this.convertSec(timeLeft));
+        })
+        
+        // Subscriber to "Stop" button
+        btnStop$.subscribe(() => {
+            counterSub.unsubscribe();
+            this.count = 0;
+        })
 
-
+        // Subscriber to "Wait" button
+        btnWaitStream$.subscribe((e) => {
+            counterSub.unsubscribe();
+        });
     }
     ngOnDestroy() {
-
+        
     }
 
 }
